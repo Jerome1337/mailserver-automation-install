42dc850a55b1ae8a86b226f3ca8e9396c00d191e
Jérôme Pogeant <jerome.pogeant@nexylan.com>
2016-12-19 18:10:22 +0100

    Remove useless installation commands

--- install.sh
+++ install.sh
@@ -8,39 +8,22 @@ import rethinkdb as r
 
 r.connect("localhost", 28015).repl()
 
-r.db_create("atlas").run()
-r.db("atlas").table_create("Carts").run()
-r.db("atlas").table_create("Checkouts").run()
-r.db("atlas").table_create("Collections").run()
-r.db("atlas").table_create("Contents").run()
-r.db("atlas").table_create("Orders").run()
-r.db("atlas").table_create("Products").run()
-r.db("atlas").table_create("Users").run()
+r.db_list().contains('atlas').do(
+    function (databaseExists) {
+        return r.branch(
+            databaseExists,
+            { dbs_created: 0 },
+            r.db_create("atlas")
+            r.db("atlas").table_create("Carts")
+            r.db("atlas").table_create("Checkouts")
+            r.db("atlas").table_create("Collections")
+            r.db("atlas").table_create("Contents")
+            r.db("atlas").table_create("Orders")
+            r.db("atlas").table_create("Products")
+            r.db("atlas").table_create("Users")
+        );
+    }
+).run()
 
 exit()
 EOF
-
-# Install and setup backend then launch it
-mkdir src
-cd src && mkdir app
-cd app
-
-git clone https://github.com/yoonic/atlas.git backend
-
-cd backend
-
-npm install
-
-SECRET="$(openssl rand -base64 32)"
-sed -i "s#JWT_KEY#JWT_KEY || \"${SECRET}\"#" config/development.js
-
-# Install and setup front then launch it
-cd ../
-
-git clone https://github.com/yoonic/nicistore.git frontend
-
-cd frontend
-
-npm install
-
-sed -i "s/api.atlas.baseUrl/http://localhost:8000/v1/" config/client/development.js
